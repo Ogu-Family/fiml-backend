9589b6b8019b89da67ca537502605fe4ca1dc0e5
hyoguoo <hyoguoo@gmail.com>
2023-12-20 10:21:47 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/kpl/fiml/FimlApplication.java
@@ -0,0 +1,13 @@
+package kpl.fiml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FimlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FimlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kpl/fiml/FimlApplicationTests.java
@@ -0,0 +1,13 @@
+package kpl.fiml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FimlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
