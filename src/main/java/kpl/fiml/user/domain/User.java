5d8a70148956b45149bde73f6a73e95ab05fa659
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-21 13:37:24 +0900

    feat: User Entity 생성 및 repository 구현

---
+++ src/main/java/kpl/fiml/user/domain/User.java
@@ -0,0 +1,49 @@
+package kpl.fiml.user.domain;
+
+import jakarta.persistence.*;
+import kpl.fiml.user.vo.ContactVo;
+import kpl.fiml.user.vo.EmailVo;
+import kpl.fiml.user.vo.PasswordVo;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "contact", nullable = false)
+    private String contact;
+
+    @Builder
+    public User(String name, String bio, String profileImage, String email, String password, String contact) {
+        this.name = name;
+        this.bio = bio;
+        this.profileImage = profileImage;
+        this.email = new EmailVo(email).getEmail();
+        this.password = new PasswordVo(password).getPassword();
+        this.contact = new ContactVo(contact).getContact();
+    }
+}
