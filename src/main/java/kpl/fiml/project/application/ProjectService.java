00ac439d518bd4f265b26c3395e716ef7fe8492c
hyoguoo <hyoguoo@gmail.com>
2023-12-26 01:34:16 +0900

    feat: 프로젝트 생성(초기화) API 추가

--- src/main/java/kpl/fiml/project/application/ProjectService.java
+++ src/main/java/kpl/fiml/project/application/ProjectService.java
@@ -1,6 +1,8 @@
 package kpl.fiml.project.application;
 
 import kpl.fiml.project.domain.ProjectRepository;
+import kpl.fiml.project.dto.ProjectInitRequest;
+import kpl.fiml.project.dto.ProjectInitResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +13,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
+
+    @Transactional
+    public ProjectInitResponse initProject(ProjectInitRequest request) {
+        return new ProjectInitResponse(projectRepository.save(request.toEntity()).getId());
+    }
 }
---
+++ src/main/java/kpl/fiml/project/dto/ProjectInitResponse.java
@@ -0,0 +1,11 @@
+package kpl.fiml.project.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ProjectInitResponse {
+
+    private final Long id;
+}
