f670b5e931cd62ee2edeab064db9acc41714e5de
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-26 19:08:23 +0900

    mod: 조회 메서드 추출

--- src/main/java/kpl/fiml/notice/application/NoticeService.java
+++ src/main/java/kpl/fiml/notice/application/NoticeService.java
@@ -23,23 +23,25 @@ public class NoticeService {
 
     @Transactional
     public NoticeUpdateResponse updateNotice(Long noticeId, NoticeUpdateRequest request) {
-        Notice notice = noticeRepository.findById(noticeId)
-                .orElseThrow(() -> new IllegalArgumentException("해당하는 공지사항이 존재하지 않습니다."));
+        Notice notice = getById(noticeId);
         notice.updateContent(request.getContent());
         return NoticeUpdateResponse.of(noticeId, notice.getContent());
     }
 
     @Transactional(readOnly = true)
     public NoticeDto findById(Long noticeId) {
-        Notice findNotice = noticeRepository.findById(noticeId)
-                .orElseThrow(() -> new IllegalArgumentException("해당하는 공지사항이 존재하지 않습니다."));
+        Notice findNotice = getById(noticeId);
         return NoticeDto.of(findNotice.getId(), findNotice.getContent(), findNotice.getCreatedAt(), findNotice.getUpdatedAt());
-
     }
 
     @Transactional
     public Long deleteById(Long noticeId) {
         noticeRepository.deleteById(noticeId);
         return noticeId;
+    }
+
+    private Notice getById(Long noticeId) {
+        return noticeRepository.findById(noticeId)
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 공지사항이 존재하지 않습니다."));
     }
 }
