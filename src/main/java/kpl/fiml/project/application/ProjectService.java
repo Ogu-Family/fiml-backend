f762a4ca8a1992623de30de81c6308dc30589084
hyoguoo <hyoguoo@gmail.com>
2023-12-26 18:25:00 +0900

    feat: 프로젝트 기본 정보 수정 API 추가

--- src/main/java/kpl/fiml/project/application/ProjectService.java
+++ src/main/java/kpl/fiml/project/application/ProjectService.java
@@ -1,6 +1,8 @@
 package kpl.fiml.project.application;
 
+import kpl.fiml.project.domain.Project;
 import kpl.fiml.project.domain.ProjectRepository;
+import kpl.fiml.project.dto.ProjectBasicInfoUpdateRequest;
 import kpl.fiml.project.dto.ProjectInitRequest;
 import kpl.fiml.project.dto.ProjectInitResponse;
 import lombok.RequiredArgsConstructor;
@@ -17,5 +19,21 @@ public class ProjectService {
     @Transactional
     public ProjectInitResponse initProject(ProjectInitRequest request) {
         return ProjectInitResponse.of(projectRepository.save(request.toEntity()).getId());
+    }
+
+    @Transactional
+    public void updateBasicInfo(Long projectId, ProjectBasicInfoUpdateRequest request) {
+        this.getProjectById(projectId)
+                .updateBasicInfo(
+                        request.getSummary(),
+                        request.getCategory(),
+                        request.getTitle(),
+                        request.getProjectImageEntities()
+                );
+    }
+
+    private Project getProjectById(Long projectId) {
+        return projectRepository.findById(projectId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 프로젝트입니다."));
     }
 }
