b65f60b2ced988f4d31816bdf9a8bc279441e33f
letskuku <yujung9288@naver.com>
2023-12-27 12:11:05 +0900

    feat: 결제하기 비즈니스 로직 구현

--- src/main/java/kpl/fiml/payment/application/PaymentService.java
+++ src/main/java/kpl/fiml/payment/application/PaymentService.java
@@ -1,9 +1,20 @@
 package kpl.fiml.payment.application;
 
+import kpl.fiml.payment.domain.Payment;
 import kpl.fiml.payment.domain.PaymentRepository;
+import kpl.fiml.payment.domain.PaymentStatus;
+import kpl.fiml.sponsor.domain.Sponsor;
+import kpl.fiml.sponsor.domain.SponsorStatus;
+import kpl.fiml.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -11,4 +22,45 @@ import org.springframework.transaction.annotation.Transactional;
 public class PaymentService {
 
     private final PaymentRepository paymentRepository;
+
+    @Scheduled(cron = "0 0 14 * * *", zone = "Asia/Seoul")
+    @Transactional
+    public void tryPay() {
+        List<Payment> payments = getRequiredPayments();
+
+        for (Payment payment : payments) {
+            Sponsor sponsor = payment.getSponsor();
+
+            try {
+                User user = sponsor.getUser();
+                Long requiredPaymentAmount = sponsor.getTotalAmount();
+                user.decreaseCash(requiredPaymentAmount);
+
+                payment.successPayed();
+                sponsor.updateStatus(SponsorStatus.COMPLETE);
+            } catch (IllegalArgumentException e) {
+                payment.failPayed();
+
+                if (paymentRepository.countBySponsor(sponsor) == 7) {
+                    sponsor.updateStatus(SponsorStatus.PAYMENT_FAIL);
+                } else {
+                    paymentRepository.save(Payment.builder()
+                            .sponsor(sponsor)
+                            .requestedAt(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(14, 0)))
+                            .build());
+                    sponsor.updateStatus(SponsorStatus.PAYMENT_PROCEEDING);
+                }
+            }
+
+        }
+    }
+
+    private List<Payment> getRequiredPayments() {
+        return paymentRepository.findAllByStatus(PaymentStatus.WAIT)
+                .stream()
+                .filter(payment ->
+                        payment.getRequestedAt().isEqual(LocalDateTime.now()) || payment.getRequestedAt().isBefore(LocalDateTime.now())
+                )
+                .toList();
+    }
 }
--- src/main/java/kpl/fiml/payment/domain/PaymentRepository.java
+++ src/main/java/kpl/fiml/payment/domain/PaymentRepository.java
@@ -1,8 +1,13 @@
 package kpl.fiml.payment.domain;
 
+import kpl.fiml.sponsor.domain.Sponsor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findAllByStatus(PaymentStatus status);
+    Long countBySponsor(Sponsor sponsor);
 }
