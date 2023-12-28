1f088aee9a8cef490c0e5ab24e9a2e02c621192c
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-28 15:53:13 +0900

    feat: password 암호화 추가

--- src/main/java/kpl/fiml/user/dto/UserCreateRequest.java
+++ src/main/java/kpl/fiml/user/dto/UserCreateRequest.java
@@ -1,8 +1,8 @@
 package kpl.fiml.user.dto;
 
 import kpl.fiml.user.domain.User;
-import lombok.Builder;
 import lombok.Getter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 public class UserCreateRequest {
@@ -13,13 +13,13 @@ public class UserCreateRequest {
     private String password;
     private String contact;
 
-    public User toEntity() {
+    public User toEntity(PasswordEncoder passwordEncoder) {
         return User.builder()
                 .name(this.name)
                 .bio(this.bio)
                 .profileImage(this.profileImage)
                 .email(this.email)
-                .password(this.password)
+                .password(passwordEncoder.encode(this.password))
                 .contact(this.contact)
                 .build();
     }
