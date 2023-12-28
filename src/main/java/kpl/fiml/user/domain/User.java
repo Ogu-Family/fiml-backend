b9723a9c0cc2e60693791c4eb89876f44b16d334
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-28 15:55:34 +0900

    feat: security UserDetails 관련 구현

--- src/main/java/kpl/fiml/user/domain/User.java
+++ src/main/java/kpl/fiml/user/domain/User.java
@@ -9,12 +9,20 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Entity
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,6 +49,9 @@ public class User extends BaseEntity {
     @Column(name = "cash", nullable = false)
     private Long cash;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
     @Builder
     public User(String name, String bio, String profileImage, String email, String password, String contact) {
         this.name = name;
@@ -50,6 +61,7 @@ public class User extends BaseEntity {
         this.password = new PasswordVo(password).getPassword();
         this.contact = new ContactVo(contact).getContact();
         this.cash = 0L;
+        this.roles.add("ROLE_USER"); // 권한 처리 필요로 하지 않아서 생성 시 기본 ROLE_USER
     }
 
     public void increaseCash(Long amount) {
@@ -67,5 +79,37 @@ public class User extends BaseEntity {
             throw new IllegalArgumentException("보유 현금이 부족합니다.");
         }
         this.cash -= amount;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
