a3147b2becc42cf7b1861c93e74c9c299f03c41d
eugene <81062608+eugene225@users.noreply.github.com>
2023-12-28 23:58:20 +0900

    feat: comment entity 생성 및 repository 구현

---
+++ src/main/java/kpl/fiml/comment/domain/Comment.java
@@ -0,0 +1,36 @@
+package kpl.fiml.comment.domain;
+
+import jakarta.persistence.*;
+import kpl.fiml.global.common.BaseEntity;
+import kpl.fiml.notice.domain.Notice;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "commenter", nullable = false)
+    private String commenter;
+
+    @ManyToOne(targetEntity = Notice.class, fetch = FetchType.LAZY, optional = false)
+    private Notice notice;
+
+    @Builder
+    public Comment(String content, String commenter, Notice notice) {
+        this.content = content;
+        this.commenter = commenter;
+        this.notice = notice;
+    }
+}
