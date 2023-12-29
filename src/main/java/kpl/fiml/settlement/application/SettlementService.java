8fd239a51247e9041d5cb42533f83090ab0c937c
letskuku <yujung9288@naver.com>
2023-12-29 10:06:45 +0900

    feat: 프로젝트별 정산 내역 조회하는 비즈니스 로직 구현

--- src/main/java/kpl/fiml/settlement/application/SettlementService.java
+++ src/main/java/kpl/fiml/settlement/application/SettlementService.java
@@ -1,6 +1,10 @@
 package kpl.fiml.settlement.application;
 
+import kpl.fiml.project.domain.Project;
+import kpl.fiml.project.domain.ProjectRepository;
+import kpl.fiml.settlement.domain.Settlement;
 import kpl.fiml.settlement.domain.SettlementRepository;
+import kpl.fiml.settlement.dto.SettlementGetResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class SettlementService {
 
     private final SettlementRepository settlementRepository;
+    private final ProjectRepository projectRepository;
+
+    public SettlementGetResponse getSettlement(Long projectId) {
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 프로젝트가 존재하지 않습니다."));
+
+        Settlement settlement = settlementRepository.findByProject(project)
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 정산 내역이 존재하지 않습니다."));
+
+        return SettlementGetResponse.of(settlement.getSettleAmount(), settlement.getSettledAt());
+    }
 }
