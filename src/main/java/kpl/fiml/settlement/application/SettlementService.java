df69092b2ccabe64ffd8d372d93a0aea9074be23
letskuku <yujung9288@naver.com>
2023-12-29 10:45:53 +0900

    feat: 정산하기 비즈니스 로직 구현

--- src/main/java/kpl/fiml/settlement/application/SettlementService.java
+++ src/main/java/kpl/fiml/settlement/application/SettlementService.java
@@ -2,20 +2,49 @@ package kpl.fiml.settlement.application;
 
 import kpl.fiml.project.domain.Project;
 import kpl.fiml.project.domain.ProjectRepository;
+import kpl.fiml.project.domain.enums.ProjectStatus;
 import kpl.fiml.settlement.domain.Settlement;
 import kpl.fiml.settlement.domain.SettlementRepository;
 import kpl.fiml.settlement.dto.SettlementGetResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SettlementService {
 
+    private static final int SETTLEMENT_OFFSET = 14;
+
     private final SettlementRepository settlementRepository;
     private final ProjectRepository projectRepository;
+
+    @Scheduled(cron = "0 0 9 * * *", zone = "Asia/Seoul")
+    @Transactional
+    public void createSettlement() {
+        List<Project> projects = projectRepository.findAllByStatus(ProjectStatus.FUNDING_COMPLETE)
+                .stream()
+                .filter(project -> project.getEndAt().plusDays(SETTLEMENT_OFFSET).isBefore(LocalDateTime.now()))
+                .toList();
+
+        for (Project project : projects) {
+            Long settlementAmount = project.getCurrentAmount();
+            project.getUser().increaseCash(settlementAmount);
+            project.updateStatusToSettlementComplete();
+
+            Settlement settlement = Settlement.builder()
+                    .project(project)
+                    .settleAmount(settlementAmount)
+                    .settledAt(LocalDateTime.now())
+                    .build();
+            settlementRepository.save(settlement);
+        }
+    }
 
     public SettlementGetResponse getSettlement(Long projectId) {
         Project project = projectRepository.findById(projectId)
