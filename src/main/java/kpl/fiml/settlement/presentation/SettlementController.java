3934f83bf39cd4770a3b77243c8be6ced8ee1209
letskuku <yujung9288@naver.com>
2023-12-29 10:06:59 +0900

    feat: 프로젝트별 정산 내역 조회 API 구현

--- src/main/java/kpl/fiml/settlement/presentation/SettlementController.java
+++ src/main/java/kpl/fiml/settlement/presentation/SettlementController.java
@@ -1,7 +1,11 @@
 package kpl.fiml.settlement.presentation;
 
 import kpl.fiml.settlement.application.SettlementService;
+import kpl.fiml.settlement.dto.SettlementGetResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class SettlementController {
 
     private final SettlementService settlementService;
+
+    @GetMapping("/settlements/{projectId}")
+    public ResponseEntity<SettlementGetResponse> getSettlement(@PathVariable Long projectId) {
+        SettlementGetResponse response = settlementService.getSettlement(projectId);
+
+        return ResponseEntity.ok(response);
+    }
 }
