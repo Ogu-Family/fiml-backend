e72c4014c1466705561d36478a942f74bcf44c63
letskuku <yujung9288@naver.com>
2023-12-30 23:07:13 +0900

    feat: 계층 간 후원 삭제 정보 이동 위한 dto 구현

---
+++ src/main/java/kpl/fiml/sponsor/dto/SponsorDeleteResponse.java
@@ -0,0 +1,19 @@
+package kpl.fiml.sponsor.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class SponsorDeleteResponse {
+
+    private final Long id;
+    private final LocalDateTime deleteAt;
+
+    public static SponsorDeleteResponse of(Long id, LocalDateTime deleteAt) {
+        return new SponsorDeleteResponse(id, deleteAt);
+    }
+}
