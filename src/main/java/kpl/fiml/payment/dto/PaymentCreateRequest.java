7d9f6dbd13c76bca18a4041593ad5ee6e83048a7
letskuku <yujung9288@naver.com>
2023-12-30 22:14:06 +0900

    feat: 계층 간 결제 요청 정보 이동 위한 dto 구현

---
+++ src/main/java/kpl/fiml/payment/dto/PaymentCreateRequest.java
@@ -0,0 +1,29 @@
+package kpl.fiml.payment.dto;
+
+import kpl.fiml.payment.domain.Payment;
+import kpl.fiml.sponsor.domain.Sponsor;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class PaymentCreateRequest {
+
+    private final Sponsor sponsor;
+    private final LocalDate requestedDay;
+
+    public static PaymentCreateRequest of(Sponsor sponsor, LocalDate requestedDay) {
+        return new PaymentCreateRequest(sponsor, requestedDay);
+    }
+
+    public Payment toEntity(LocalDateTime requestedAt) {
+        return Payment.builder()
+                .sponsor(sponsor)
+                .requestedAt(requestedAt)
+                .build();
+    }
+}
