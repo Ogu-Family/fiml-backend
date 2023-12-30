80f8fb4827299a3fd14ba427da4acb3e19b20480
letskuku <yujung9288@naver.com>
2023-12-30 23:21:46 +0900

    refactor: 포맷팅 정리

--- src/main/java/kpl/fiml/sponsor/application/SponsorService.java
+++ src/main/java/kpl/fiml/sponsor/application/SponsorService.java
@@ -35,8 +35,8 @@ public class SponsorService {
                 .orElseThrow(() -> new IllegalArgumentException("해당하는 리워드가 존재하지 않습니다."));
 
         Sponsor sponsor = sponsorRepository.save(request.toEntity(user, reward));
-        reward.getProject().updateSponsorAddInfo(sponsor.getTotalAmount());
 
+        reward.getProject().updateSponsorAddInfo(sponsor.getTotalAmount());
         paymentService.createPayment(PaymentCreateRequest.of(sponsor, reward.getProject().getEndAt().toLocalDate()));
 
         return SponsorCreateResponse.of(sponsor.getId());
