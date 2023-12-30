0701a725bf2c089a9c16cc2cc5d19d7c2472c1ff
letskuku <yujung9288@naver.com>
2023-12-31 02:08:13 +0900

    chore: 필요없는 메서드 삭제

--- src/main/java/kpl/fiml/sponsor/domain/SponsorRepository.java
+++ src/main/java/kpl/fiml/sponsor/domain/SponsorRepository.java
@@ -1,15 +1,12 @@
 package kpl.fiml.sponsor.domain;
 
 import kpl.fiml.project.domain.Reward;
-import kpl.fiml.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface SponsorRepository extends JpaRepository<Sponsor, Long> {
-    Optional<Sponsor> findByIdAndUser(Long id, User user);
     List<Sponsor> findAllByReward(Reward reward);
 }
