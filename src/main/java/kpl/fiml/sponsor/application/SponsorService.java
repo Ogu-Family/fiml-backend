17b57c28cbdda58fa2458e18fef2baf305c0d547
letskuku <yujung9288@naver.com>
2023-12-30 22:49:43 +0900

    feat: 후원 삭제 시 펀딩 실패로 상태 변경하는 로직 추가

--- src/main/java/kpl/fiml/sponsor/application/SponsorService.java
+++ src/main/java/kpl/fiml/sponsor/application/SponsorService.java
@@ -48,6 +48,7 @@ public class SponsorService {
 
             for (Sponsor sponsor : sponsors) {
                 paymentService.deletePayments(sponsor);
+                sponsor.updateStatusToFundingFail();
                 sponsor.delete();
             }
         }
