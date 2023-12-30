0b7aa466fea66f6605562bd2b2fa8634fa7a837e
letskuku <yujung9288@naver.com>
2023-12-30 22:38:55 +0900

    feat: 프로젝트에 대한 후원 정보 삭제하는 비즈니스 로직 구현

--- src/main/java/kpl/fiml/sponsor/application/SponsorService.java
+++ src/main/java/kpl/fiml/sponsor/application/SponsorService.java
@@ -2,6 +2,7 @@ package kpl.fiml.sponsor.application;
 
 import kpl.fiml.payment.application.PaymentService;
 import kpl.fiml.payment.dto.PaymentCreateRequest;
+import kpl.fiml.project.domain.Project;
 import kpl.fiml.project.domain.Reward;
 import kpl.fiml.project.domain.RewardRepository;
 import kpl.fiml.sponsor.domain.Sponsor;
@@ -12,6 +13,8 @@ import kpl.fiml.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -34,5 +37,19 @@ public class SponsorService {
         paymentService.createPayment(PaymentCreateRequest.of(sponsor, reward.getProject().getEndAt().toLocalDate()));
 
         return SponsorCreateResponse.of(sponsor.getId());
+    }
+
+    @Transactional
+    public void deleteSponsors(Project project) {
+        List<Reward> rewards = rewardRepository.findAllByProject(project);
+
+        for (Reward reward : rewards) {
+            List<Sponsor> sponsors = sponsorRepository.findAllByReward(reward);
+
+            for (Sponsor sponsor : sponsors) {
+                paymentService.deletePayments(sponsor);
+                sponsor.delete();
+            }
+        }
     }
 }
