77ffdec18e1596ab72acfebae17183adfea5fbe7
letskuku <yujung9288@naver.com>
2023-12-30 23:12:24 +0900

    feat: 후원자 요청으로 후원 취소하는 비즈니스 로직 구현

--- src/main/java/kpl/fiml/sponsor/application/SponsorService.java
+++ src/main/java/kpl/fiml/sponsor/application/SponsorService.java
@@ -9,11 +9,13 @@ import kpl.fiml.sponsor.domain.Sponsor;
 import kpl.fiml.sponsor.domain.SponsorRepository;
 import kpl.fiml.sponsor.dto.SponsorCreateRequest;
 import kpl.fiml.sponsor.dto.SponsorCreateResponse;
+import kpl.fiml.sponsor.dto.SponsorDeleteResponse;
 import kpl.fiml.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -40,7 +42,22 @@ public class SponsorService {
     }
 
     @Transactional
-    public void deleteSponsors(Project project) {
+    public SponsorDeleteResponse deleteSponsorByUser(Long sponsorId, User user) {
+        Sponsor sponsor = sponsorRepository.findByIdAndUser(sponsorId, user)
+                .filter(uncheckedSponsor -> !uncheckedSponsor.isDeleted())
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 후원이 존재하지 않습니다."));
+
+        if (sponsor.getReward().getProject().getEndAt().isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("펀딩이 종료된 후에는 후원 취소가 불가합니다.");
+        }
+
+        sponsor.delete();
+
+        return SponsorDeleteResponse.of(sponsor.getId(), sponsor.getDeletedAt());
+    }
+
+    @Transactional
+    public void deleteSponsorsByProject(Project project) {
         List<Reward> rewards = rewardRepository.findAllByProject(project);
 
         for (Reward reward : rewards) {
