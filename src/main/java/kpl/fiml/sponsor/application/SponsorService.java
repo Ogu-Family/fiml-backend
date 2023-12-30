5539a558ffd9bd5c88fed84e5a4de8013561abc3
letskuku <yujung9288@naver.com>
2023-12-30 22:17:24 +0900

    feat: 후원 생성 비즈니스 로직 구현

--- src/main/java/kpl/fiml/sponsor/application/SponsorService.java
+++ src/main/java/kpl/fiml/sponsor/application/SponsorService.java
@@ -1,6 +1,14 @@
 package kpl.fiml.sponsor.application;
 
+import kpl.fiml.payment.application.PaymentService;
+import kpl.fiml.payment.dto.PaymentCreateRequest;
+import kpl.fiml.project.domain.Reward;
+import kpl.fiml.project.domain.RewardRepository;
+import kpl.fiml.sponsor.domain.Sponsor;
 import kpl.fiml.sponsor.domain.SponsorRepository;
+import kpl.fiml.sponsor.dto.SponsorCreateRequest;
+import kpl.fiml.sponsor.dto.SponsorCreateResponse;
+import kpl.fiml.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,5 +18,21 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SponsorService {
 
+    private final PaymentService paymentService;
+
     private final SponsorRepository sponsorRepository;
+    private final RewardRepository rewardRepository;
+
+    @Transactional
+    public SponsorCreateResponse createSponsor(SponsorCreateRequest request, User user) {
+        Reward reward = rewardRepository.findById(request.getRewardId())
+                .filter(uncheckedReward -> !uncheckedReward.isDeleted())
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 리워드가 존재하지 않습니다."));
+
+        Sponsor sponsor = sponsorRepository.save(request.toEntity(user, reward));
+
+        paymentService.createPayment(PaymentCreateRequest.of(sponsor, reward.getProject().getEndAt().toLocalDate()));
+
+        return SponsorCreateResponse.of(sponsor.getId());
+    }
 }
