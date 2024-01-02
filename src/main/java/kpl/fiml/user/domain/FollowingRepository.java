6ba0cd91488a7d12d148a4a39d1d6d97b406efb7
hyoguoo <hyoguoo@gmail.com>
2024-01-02 22:07:52 +0900

    feat: FollowingRepository 생성

---
+++ src/main/java/kpl/fiml/user/domain/FollowingRepository.java
@@ -0,0 +1,11 @@
+package kpl.fiml.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FollowingRepository extends JpaRepository<Following, Long> {
+    Optional<Following> findByFollowingUserAndFollowerUser(User following, User follower);
+}
