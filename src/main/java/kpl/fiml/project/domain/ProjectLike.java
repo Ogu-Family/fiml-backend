a566ff9c127fb9f31b07ebfa01fc9a09b9ca0302
hyoguoo <hyoguoo@gmail.com>
2024-01-02 21:44:04 +0900

    feat: Project Like Entity 및 Repository 생성

---
+++ src/main/java/kpl/fiml/project/domain/ProjectLike.java
@@ -0,0 +1,33 @@
+package kpl.fiml.project.domain;
+
+import jakarta.persistence.*;
+import kpl.fiml.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "project_likes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProjectLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public ProjectLike(Project project, User user) {
+        this.project = project;
+        this.user = user;
+    }
+}
---
+++ src/main/java/kpl/fiml/project/domain/ProjectLikeRepository.java
@@ -0,0 +1,12 @@
+package kpl.fiml.project.domain;
+
+import kpl.fiml.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProjectLikeRepository extends JpaRepository<ProjectLike, Long> {
+    Optional<ProjectLike> findByProjectAndUser(Project project, User user);
+}
