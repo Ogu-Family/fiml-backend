547a0a8b1fc50acf041e5fb520672f88ede2f79b
hyoguoo <hyoguoo@gmail.com>
2024-01-04 06:50:12 +0900

    feat: 프로젝트 수수료율 검증 로직 추가

--- src/main/java/kpl/fiml/project/dto/ProjectFundingPlanUpdateRequest.java
+++ src/main/java/kpl/fiml/project/dto/ProjectFundingPlanUpdateRequest.java
@@ -11,6 +11,5 @@ public class ProjectFundingPlanUpdateRequest {
     private Long goalAmount;
     private LocalDateTime fundingStartDateTime;
     private LocalDate fundingEndDate; // 저장 시 시간 23:59:59로 고정
-    // TODO: 0-100 실수 범위 검증 추가
     private Double commissionRate;
 }
