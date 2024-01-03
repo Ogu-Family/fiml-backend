b1f9b72503c17ebc4f604597a2d168a6dbbd9174
hyoguoo <hyoguoo@gmail.com>
2024-01-04 07:36:20 +0900

    feat: 프로젝트 이미지 업로드 시 path NotNull 설정

--- src/main/java/kpl/fiml/project/dto/ProjectImageDto.java
+++ src/main/java/kpl/fiml/project/dto/ProjectImageDto.java
@@ -1,11 +1,13 @@
 package kpl.fiml.project.dto;
 
+import jakarta.validation.constraints.NotNull;
 import kpl.fiml.project.domain.ProjectImage;
 import lombok.Getter;
 
 @Getter
 public class ProjectImageDto {
 
+    @NotNull
     private String path;
 
     public ProjectImage toEntity(int listIndex) {
