e594c991e68fe83d9ce77bc11ee2dc4b04c38073
letskuku <yujung9288@naver.com>
2024-01-03 11:37:53 +0900

    feat: Sponsor 조회 시 deletedAt null 확인하는 메서드 추가

--- src/main/java/kpl/fiml/sponsor/domain/SponsorRepository.java
+++ src/main/java/kpl/fiml/sponsor/domain/SponsorRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SponsorRepository extends JpaRepository<Sponsor, Long> {
+    Optional<Sponsor> findByIdAndDeletedAtIsNull(Long id);
     List<Sponsor> findAllByReward(Reward reward);
 }
