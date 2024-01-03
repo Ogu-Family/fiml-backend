2e8bc0e0295cbe38ffaf6c14b2f8351d27d79c6b
hyoguoo <hyoguoo@gmail.com>
2024-01-04 06:42:01 +0900

    feat: 프로젝트 리워드 생성 요청 시 모든 값 NotNull로 변경

--- src/main/java/kpl/fiml/project/dto/ProjectRewardDto.java
+++ src/main/java/kpl/fiml/project/dto/ProjectRewardDto.java
@@ -1,5 +1,6 @@
 package kpl.fiml.project.dto;
 
+import jakarta.validation.constraints.NotNull;
 import kpl.fiml.project.domain.Reward;
 import lombok.Getter;
 
@@ -8,13 +9,19 @@ import java.time.LocalDate;
 @Getter
 public class ProjectRewardDto {
 
+    @NotNull
     private String title;
+    @NotNull
     private String content;
+    @NotNull
     private Long price;
+    @NotNull
     private Boolean quantityLimited;
+    @NotNull
     private Integer quantity;
+    @NotNull
     private LocalDate deliveryDate;
-    // TODO: 1 이상의 정수 범위 검증 추가
+    @NotNull
     private Integer maxPurchaseQuantity;
 
     public Reward toEntity(int listIndex) {
