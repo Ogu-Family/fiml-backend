977fe905dbb876521dd2481c7ee0ba2c091fca9b
eugene <81062608+eugene225@users.noreply.github.com>
2024-01-05 02:13:01 +0900

    mod: userCreateRequest Builder 추가

--- src/main/java/kpl/fiml/user/dto/request/UserCreateRequest.java
+++ src/main/java/kpl/fiml/user/dto/request/UserCreateRequest.java
@@ -3,9 +3,11 @@ package kpl.fiml.user.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import kpl.fiml.user.domain.User;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class UserCreateRequest {
     @NotBlank(message = "이름은 필수 입력값 입니다.")
     private String name;
