0969b120b05be6ff744ca08a122ee221e7d986e2
letskuku <yujung9288@naver.com>
2024-01-07 22:12:18 +0900

    test: 후원 도메인 테스트 작성

---
+++ src/test/java/kpl/fiml/sponsor/domain/SponsorTest.java
@@ -0,0 +1,34 @@
+package kpl.fiml.sponsor.domain;
+
+import kpl.fiml.project.domain.Reward;
+import kpl.fiml.user.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class SponsorTest {
+
+    @Test
+    @DisplayName("후원 생성 시 리워드 가격보다 적은 후원 금액은 예외를 발생시킨다.")
+    void testInvalidTotalAmountException() {
+        // given
+        User user = User.builder()
+                .email("abc@gmail.com")
+                .contact("01012345678")
+                .build();
+        Reward reward = Reward.builder()
+                .quantityLimited(false)
+                .maxPurchaseQuantity(100000)
+                .price(50000L)
+                .build();
+
+        Long totalAmount = 49999L;
+
+        // when-then
+        assertThatThrownBy(() -> Sponsor.builder().user(user).reward(reward).totalAmount(totalAmount).build())
+                .isInstanceOf(IllegalArgumentException.class) // TODO 커스텀 예외 생성 시 변경 필요
+                .hasMessageContaining("후원 금액이 리워드 가격보다 적습니다.");
+    }
+
+}
