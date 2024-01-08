b3049001911a1fe751be580bff14779b4e637467
hyoguoo <hyoguoo@gmail.com>
2024-01-09 08:56:09 +0900

    fix: MockMvc에서 DTO 객체 올바르게 생성하도록 수정

--- src/main/java/kpl/fiml/project/dto/request/ProjectDetailIntroductionUpdateRequest.java
+++ src/main/java/kpl/fiml/project/dto/request/ProjectDetailIntroductionUpdateRequest.java
@@ -1,10 +1,14 @@
 package kpl.fiml.project.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectDetailIntroductionUpdateRequest {
 
     private String introduction;
