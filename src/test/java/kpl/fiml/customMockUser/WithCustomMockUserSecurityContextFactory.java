838a04fbd7eb2fdbf77fa9e432990a0341ba1dc5
letskuku <yujung9288@naver.com>
2024-01-08 21:16:40 +0900

    test: 테스트에서 AuthenticationPrincipal 주입 위한 커스텀 MockUser 구현

---
+++ src/test/java/kpl/fiml/customMockUser/WithCustomMockUserSecurityContextFactory.java
@@ -0,0 +1,39 @@
+package kpl.fiml.customMockUser;
+
+import kpl.fiml.user.domain.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+public class WithCustomMockUserSecurityContextFactory implements WithSecurityContextFactory<WithCustomMockUser> {
+    @Override
+    public SecurityContext createSecurityContext(WithCustomMockUser annotation) {
+        Long id = 1L;
+        String email = annotation.email();
+        String contact = annotation.contact();
+
+        User user = User.builder()
+                .email(email)
+                .contact(contact)
+                .build();
+        try {
+            Field userId = user.getClass().getDeclaredField("id");
+            userId.setAccessible(true);
+            userId.set(user, id);
+        } catch (NoSuchFieldException | IllegalAccessException ignored) {
+
+        }
+
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(user, "password", List.of(new SimpleGrantedAuthority("USER")));
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(token);
+
+        return context;
+    }
+}
