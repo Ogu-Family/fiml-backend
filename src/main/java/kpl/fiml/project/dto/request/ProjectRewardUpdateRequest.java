5e4e2de54abecdaec7f4158deef359bd7924d37f
hyoguoo <hyoguoo@gmail.com>
2024-01-09 08:56:09 +0900

    fix: MockMvc에서 DTO 객체 올바르게 생성하도록 수정

--- src/main/java/kpl/fiml/project/dto/request/ProjectRewardUpdateRequest.java
+++ src/main/java/kpl/fiml/project/dto/request/ProjectRewardUpdateRequest.java
@@ -2,13 +2,17 @@ package kpl.fiml.project.dto.request;
 
 import jakarta.validation.Valid;
 import kpl.fiml.project.domain.Reward;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectRewardUpdateRequest {
 
     private List<@Valid ProjectRewardRequest> rewards;
