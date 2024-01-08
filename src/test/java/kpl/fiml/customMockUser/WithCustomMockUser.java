ba193075f062276c25eb15604436a73f8fda0845
letskuku <yujung9288@naver.com>
2024-01-08 21:16:40 +0900

    test: 테스트에서 AuthenticationPrincipal 주입 위한 커스텀 MockUser 구현

---
+++ src/test/java/kpl/fiml/customMockUser/WithCustomMockUser.java
@@ -0,0 +1,13 @@
+package kpl.fiml.customMockUser;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithCustomMockUserSecurityContextFactory.class)
+public @interface WithCustomMockUser {
+    String email() default "aaa@gmail.com";
+    String contact() default "01012345678";
+}
