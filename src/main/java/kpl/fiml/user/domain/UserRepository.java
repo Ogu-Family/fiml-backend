c8ba8b7fbdc3dfc51d53eaafac1b480caedbd203
eugene <81062608+eugene225@users.noreply.github.com>
2024-01-09 20:00:19 +0900

    feat: user 조회 시 user_role 조회 쿼리 발생 안하도록 fetch join 적용

--- src/main/java/kpl/fiml/user/domain/UserRepository.java
+++ src/main/java/kpl/fiml/user/domain/UserRepository.java
@@ -1,13 +1,19 @@
 package kpl.fiml.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByIdAndDeletedAtIsNull(Long id);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.projectList WHERE u.id = :userId AND u.deletedAt IS NULL")
+    Optional<User> findByIdAndDeletedAtIsNull(@Param("userId") Long id);
+
     Optional<User> findByEmailAndDeletedAtIsNull(String email);
+
     boolean existsByEmailAndDeletedAtIsNull(String email);
 }
