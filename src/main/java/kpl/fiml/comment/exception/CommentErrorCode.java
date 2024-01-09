0231e59cb6b273bca329a68a5b367be4c910e8d4
eugene <81062608+eugene225@users.noreply.github.com>
2024-01-10 00:13:22 +0900

    feat: commentGlobalException 생성 및 CommentErrorCode 이넘 생성

---
+++ src/main/java/kpl/fiml/comment/exception/CommentErrorCode.java
@@ -0,0 +1,14 @@
+package kpl.fiml.comment.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommentErrorCode {
+    COMMENT_NOT_FOUND("COMMENT_NOT_FOUND", "해당 댓글이 존재하지 않습니다."),
+    ACCESS_DENIED("ACCESS_DENIED", "Comment 접근 권한이 없습니다.");
+
+    private final String code;
+    private final String message;
+}
---
+++ src/main/java/kpl/fiml/comment/exception/CommentGlobalException.java
@@ -0,0 +1,7 @@
+package kpl.fiml.comment.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CommentGlobalException {
+}
