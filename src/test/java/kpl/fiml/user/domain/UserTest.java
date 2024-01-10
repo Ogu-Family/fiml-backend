65c9443633aad73c2e00409266a51b82992fcfa8
eugene <81062608+eugene225@users.noreply.github.com>
2024-01-10 23:38:44 +0900

    test: user domain cash 관련 테스트

--- src/test/java/kpl/fiml/user/domain/UserTest.java
+++ src/test/java/kpl/fiml/user/domain/UserTest.java
@@ -1,6 +1,8 @@
 package kpl.fiml.user.domain;
 
 import kpl.fiml.TestDataFactory;
+import kpl.fiml.user.exception.CashNotEnoughException;
+import kpl.fiml.user.exception.InvalidAmountException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -10,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
 
     @Test
-    @DisplayName("사용자 캐시 증가 성공")
+    @DisplayName("사용자 캐시 증가 성공: 유효한 금액")
     void testIncreaseCash_Success_ValidAmount() {
         // Given
         User user = TestDataFactory.generateUserWithId(1L);
@@ -35,4 +37,26 @@ class UserTest {
         // Then
         assertEquals(100L, user.getCash());
     }
+
+    @Test
+    @DisplayName("사용자 캐시 감소 실패: 부적절한 금액")
+    void testDecreaseCash_Fail_InvalidAmount() {
+        // Given
+        User user = TestDataFactory.generateUserWithId(1L);
+
+        // When/Then
+        assertThrows(InvalidAmountException.class, () -> user.decreaseCash(-100L), "부적절한 금액으로 인해 예외가 발생해야 합니다.");
+    }
+
+    @Test
+    @DisplayName("사용자 캐시 감소 실패: 캐시 부족")
+    void testDecreaseCash_Fail_NotEnoughCash() {
+        // Given
+        User user = TestDataFactory.generateUserWithId(1L);
+        ReflectionTestUtils.setField(user, "cash", 50L);
+
+        // When/Then
+        assertThrows(CashNotEnoughException.class, () -> user.decreaseCash(100L), "캐시 부족으로 인해 예외가 발생해야 합니다.");
+    }
+
 }
