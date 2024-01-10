f5edf68b30c645f145c1a6d570e684b83adcf46d
eugene <81062608+eugene225@users.noreply.github.com>
2024-01-11 02:17:57 +0900

    test: user service 접근 권한 없음 생성 실패 테스트

--- src/test/java/kpl/fiml/notice/application/NoticeServiceTest.java
+++ src/test/java/kpl/fiml/notice/application/NoticeServiceTest.java
@@ -5,6 +5,7 @@ import kpl.fiml.notice.domain.Notice;
 import kpl.fiml.notice.domain.NoticeRepository;
 import kpl.fiml.notice.dto.request.NoticeCreateRequest;
 import kpl.fiml.notice.dto.response.NoticeCreateResponse;
+import kpl.fiml.notice.exception.NoticePermissionException;
 import kpl.fiml.project.application.ProjectService;
 import kpl.fiml.project.domain.Project;
 import kpl.fiml.user.application.UserService;
@@ -17,8 +18,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -73,5 +73,32 @@ class NoticeServiceTest {
         verify(userService, times(1)).getById(userId);
         verify(projectService, times(1)).getProjectByIdWithUser(projectId);
         verify(noticeRepository, times(1)).save(any(Notice.class));
+    }
+
+    @Test
+    @DisplayName("공지사항 생성 실패 : 접근 권한 없음")
+    void testCreateNotice_Fail_AccessDenied() {
+        // Given
+        Long userId = 1L;
+        Long projectId = 2L;
+
+        User user = TestDataFactory.generateUserWithId(userId);
+        Project project = TestDataFactory.generateDefaultProject(projectId);
+
+        NoticeCreateRequest request = NoticeCreateRequest.builder()
+                .content("Notice Content")
+                .projectId(projectId)
+                .build();
+
+        when(userService.getById(userId)).thenReturn(user);
+        when(projectService.getProjectByIdWithUser(projectId)).thenReturn(project);
+
+        // When/Then
+        assertThrows(NoticePermissionException.class, () -> noticeService.createNotice(userId, request));
+
+        // Verify
+        verify(userService, times(1)).getById(userId);
+        verify(projectService, times(1)).getProjectByIdWithUser(projectId);
+        verify(noticeRepository, never()).save(any(Notice.class));
     }
 }
