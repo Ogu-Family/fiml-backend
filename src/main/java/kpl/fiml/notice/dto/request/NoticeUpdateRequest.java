63ea72753cf92fc2efb1cf0469924303b31d31af
eugene <81062608+eugene225@users.noreply.github.com>
2024-01-10 10:10:39 +0900

    mod: request dto access level private 변환

--- src/main/java/kpl/fiml/notice/dto/request/NoticeUpdateRequest.java
+++ src/main/java/kpl/fiml/notice/dto/request/NoticeUpdateRequest.java
@@ -1,15 +1,12 @@
 package kpl.fiml.notice.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class NoticeUpdateRequest {
 
     @NotBlank(message = "공지사항 내용은 필수 입력 값 입니다.")
