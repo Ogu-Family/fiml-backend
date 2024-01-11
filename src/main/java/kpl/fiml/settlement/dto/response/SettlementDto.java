a852509af0df207d0350813caa0cf31c0a6aca79
letskuku <yujung9288@naver.com>
2024-01-12 04:12:31 +0900

    docs: 정산 controller, dto에 swagger 적용

--- src/main/java/kpl/fiml/settlement/dto/response/SettlementDto.java
+++ src/main/java/kpl/fiml/settlement/dto/response/SettlementDto.java
@@ -1,5 +1,6 @@
 package kpl.fiml.settlement.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,10 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class SettlementDto {
 
+    @Schema(description = "총 정산금", example = "1985724587")
     private final Long settleAmount;
+
+    @Schema(description = "정산 처리 일시")
     private final LocalDateTime settledAt;
 
     public static SettlementDto of(Long settleAmount, LocalDateTime settledAt) {
--- src/main/java/kpl/fiml/settlement/presentation/SettlementController.java
+++ src/main/java/kpl/fiml/settlement/presentation/SettlementController.java
@@ -1,5 +1,11 @@
 package kpl.fiml.settlement.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kpl.fiml.settlement.application.SettlementService;
 import kpl.fiml.settlement.dto.response.SettlementDto;
 import kpl.fiml.user.domain.User;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Settlement Controller", description = "정산 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
@@ -18,6 +25,17 @@ public class SettlementController {
 
     private final SettlementService settlementService;
 
+    @Operation(summary = "정산 조회", description = "특정 프로젝트에 대한 정산 내역 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "정산 내역 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "다른 창작자의 프로젝트에 대한 결제 조회 시도"),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 회원으로 요청됨"),
+            @ApiResponse(responseCode = "403", description = "접근 권한이 없는 회원으로 요청됨"),
+            @ApiResponse(responseCode = "404", description = "1. 존재하지 않는 회원 아이디로 요청됨\n 2. 존재하지 않는 프로젝트 아이디로 요청됨")
+    })
+    @Parameters({
+            @Parameter(name = "projectId", description = "프로젝트 아이디")
+    })
     @GetMapping("/settlements/{projectId}")
     public ResponseEntity<SettlementDto> getSettlementByProject(@PathVariable Long projectId, @AuthenticationPrincipal User user) {
         SettlementDto response = settlementService.getSettlementByProject(projectId, user.getId());
