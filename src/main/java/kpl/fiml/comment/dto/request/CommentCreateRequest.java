21ec4cc977da49e300e6cffc99b229eb6edb32e1
eugene <81062608+eugene225@users.noreply.github.com>
2024-01-12 01:43:01 +0900

    docs: comment dto 스웨거 적용

--- src/main/java/kpl/fiml/comment/dto/request/CommentCreateRequest.java
+++ src/main/java/kpl/fiml/comment/dto/request/CommentCreateRequest.java
@@ -1,16 +1,19 @@
 package kpl.fiml.comment.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import kpl.fiml.comment.domain.Comment;
 import kpl.fiml.notice.domain.Notice;
 import kpl.fiml.user.domain.User;
 import lombok.*;
 
+@Schema(description = "댓글 생성 요청 DTO")
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommentCreateRequest {
+    @Schema(description = "댓글 내용", example = "comment content")
     @NotBlank(message = "댓글 내용은 필수 입력값 입니다.")
     private String content;
 
--- src/main/java/kpl/fiml/comment/dto/response/CommentCreateResponse.java
+++ src/main/java/kpl/fiml/comment/dto/response/CommentCreateResponse.java
@@ -1,15 +1,21 @@
 package kpl.fiml.comment.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Schema(description = "댓글 생성 응답 DTO")
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommentCreateResponse {
+    @Schema(description = "생성된 댓글 id", example = "1")
     private final Long id;
+    @Schema(description = "생성된 댓글 내용", example = "comment content")
     private final String content;
+    @Schema(description = "생성된 댓글 작성자 id", example = "1")
     private final Long userId;
+    @Schema(description = "생성된 댓글의 공지사항 id", example = "1")
     private final Long noticeId;
 
     public static CommentCreateResponse of(Long id, String content, Long userId, Long noticeId) {
