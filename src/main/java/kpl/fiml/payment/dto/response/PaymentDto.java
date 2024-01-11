d2d3586a02920b998cb04dbbffbbe00144d7bf68
letskuku <yujung9288@naver.com>
2024-01-11 10:47:15 +0900

    docs: 결제 controller, dto에 swagger 적용
    Co-authored-by: hyoguoo <hyoguoo@gmail.com>
    Co-authored-by: eugene <81062608+eugene225@users.noreply.github.com>

--- src/main/java/kpl/fiml/payment/dto/response/PaymentDto.java
+++ src/main/java/kpl/fiml/payment/dto/response/PaymentDto.java
@@ -1,17 +1,24 @@
 package kpl.fiml.payment.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "결제 정보 DTO")
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class PaymentDto {
 
+    @Schema(description = "결제 성공/실패 여부", example = "결제 성공")
     private final String paymentStatus;
+
+    @Schema(description = "결제 요청 일시")
     private final LocalDateTime requestedAt;
+
+    @Schema(description = "결제 승인 일시")
     private final LocalDateTime approvedAt;
 
     public static PaymentDto of(String paymentStatus, LocalDateTime requestedAt, LocalDateTime approvedAt) {
