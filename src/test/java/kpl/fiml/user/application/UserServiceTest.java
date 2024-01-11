26dad262dbda24de29b7f7bf278b1ad177c202a0
hyoguoo <hyoguoo@gmail.com>
2024-01-12 02:15:01 +0900

    test: 유저 팔로우 서비스 테스트 코드 추가

--- src/test/java/kpl/fiml/user/application/UserServiceTest.java
+++ src/test/java/kpl/fiml/user/application/UserServiceTest.java
@@ -2,6 +2,8 @@ package kpl.fiml.user.application;
 
 import kpl.fiml.TestDataFactory;
 import kpl.fiml.global.jwt.JwtTokenProvider;
+import kpl.fiml.user.domain.Following;
+import kpl.fiml.user.domain.FollowingRepository;
 import kpl.fiml.user.domain.User;
 import kpl.fiml.user.domain.UserRepository;
 import kpl.fiml.user.dto.request.LoginRequest;
@@ -31,6 +33,9 @@ public class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private FollowingRepository followingRepository;
 
     @Mock
     private JwtTokenProvider jwtTokenProvider;
@@ -195,5 +200,101 @@ public class UserServiceTest {
         verify(userRepository, times(1)).findByIdAndDeletedAtIsNull(loginUserId);
         assert fakeUser != null;
         verify(userRepository, never()).delete(fakeUser);
+    }
+
+    @Test
+    @DisplayName("팔로우 성공")
+    void testFollow_success() {
+        // Given
+        Long followingId = 1L;
+        Long followerId = 2L;
+        User following = TestDataFactory.generateUserWithId(followingId);
+        User follower = TestDataFactory.generateUserWithId(followerId);
+
+        when(userRepository.findByIdAndDeletedAtIsNull(followingId)).thenReturn(Optional.ofNullable(following));
+        when(userRepository.findByIdAndDeletedAtIsNull(followerId)).thenReturn(Optional.ofNullable(follower));
+        when(followingRepository.existsByFollowingUserAndFollowerUser(following, follower)).thenReturn(false);
+
+        // When
+        userService.follow(followingId, followerId);
+
+        // Then
+        verify(userRepository, times(1)).findByIdAndDeletedAtIsNull(followingId);
+        verify(userRepository, times(1)).findByIdAndDeletedAtIsNull(followerId);
+        verify(followingRepository, times(1)).existsByFollowingUserAndFollowerUser(following, follower);
+        verify(followingRepository, times(1)).save(any());
+    }
+
+    @Test
+    @DisplayName("팔로우 실패 : 이미 팔로우한 사용자")
+    void testFollow_fail_AlreadyFollowing() {
+        // Given
+        Long followingId = 1L;
+        Long followerId = 2L;
+        User following = TestDataFactory.generateUserWithId(followingId);
+        User follower = TestDataFactory.generateUserWithId(followerId);
+
+        when(userRepository.findByIdAndDeletedAtIsNull(followingId)).thenReturn(Optional.ofNullable(following));
+        when(userRepository.findByIdAndDeletedAtIsNull(followerId)).thenReturn(Optional.ofNullable(follower));
+        when(followingRepository.existsByFollowingUserAndFollowerUser(following, follower)).thenReturn(true);
+
+        // When/Then
+        assertThrows(FollowException.class, () -> userService.follow(followingId, followerId));
+
+        // verify
+        verify(userRepository, times(1)).findByIdAndDeletedAtIsNull(followingId);
+        verify(userRepository, times(1)).findByIdAndDeletedAtIsNull(followerId);
+        verify(followingRepository, times(1)).existsByFollowingUserAndFollowerUser(following, follower);
+        verify(followingRepository, never()).save(any());
+    }
+
+    @Test
+    @DisplayName("언팔로우 성공")
+    void testUnfollow_success() {
+        // Given
+        Long followingId = 1L;
+        Long followerId = 2L;
+        User following = TestDataFactory.generateUserWithId(followingId);
+        User follower = TestDataFactory.generateUserWithId(followerId);
+        Following followingRelationship = Following.builder()
+                .followingUser(following)
+                .followerUser(follower)
+                .build();
+
+        when(userRepository.findByIdAndDeletedAtIsNull(followingId)).thenReturn(Optional.ofNullable(following));
+        when(userRepository.findByIdAndDeletedAtIsNull(followerId)).thenReturn(Optional.ofNullable(follower));
+        when(followingRepository.findByFollowingUserAndFollowerUser(following, follower)).thenReturn(Optional.ofNullable(followingRelationship));
+
+        // When
+        userService.unfollow(followingId, followerId);
+
+        // Then
+        verify(userRepository, times(1)).findByIdAndDeletedAtIsNull(followingId);
+        verify(userRepository, times(1)).findByIdAndDeletedAtIsNull(followerId);
+        verify(followingRepository, times(1)).findByFollowingUserAndFollowerUser(following, follower);
+        verify(followingRepository, times(1)).delete(any());
+    }
+
+    @Test
+    @DisplayName("언팔로우 실패 : 팔로우하지 않은 사용자")
+    void testUnfollow_fail_NotFollowing() {
+        // Given
+        Long followingId = 1L;
+        Long followerId = 2L;
+        User following = TestDataFactory.generateUserWithId(followingId);
+        User follower = TestDataFactory.generateUserWithId(followerId);
+
+        when(userRepository.findByIdAndDeletedAtIsNull(followingId)).thenReturn(Optional.ofNullable(following));
+        when(userRepository.findByIdAndDeletedAtIsNull(followerId)).thenReturn(Optional.ofNullable(follower));
+        when(followingRepository.findByFollowingUserAndFollowerUser(following, follower)).thenReturn(Optional.empty());
+
+        // When/Then
+        assertThrows(FollowException.class, () -> userService.unfollow(followingId, followerId));
+
+        // verify
+        verify(userRepository, times(1)).findByIdAndDeletedAtIsNull(followingId);
+        verify(userRepository, times(1)).findByIdAndDeletedAtIsNull(followerId);
+        verify(followingRepository, times(1)).findByFollowingUserAndFollowerUser(following, follower);
+        verify(followingRepository, never()).delete(any());
     }
 }
