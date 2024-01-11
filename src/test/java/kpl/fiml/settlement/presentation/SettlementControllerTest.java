68809ba920c5535d3d372b9129b3c991689cda46
letskuku <yujung9288@naver.com>
2024-01-12 05:16:21 +0900

    refactor: 정산 컨트롤러 테스트 리팩토링

--- src/test/java/kpl/fiml/settlement/presentation/SettlementControllerTest.java
+++ src/test/java/kpl/fiml/settlement/presentation/SettlementControllerTest.java
@@ -1,13 +1,25 @@
 package kpl.fiml.settlement.presentation;
 
 import kpl.fiml.customMockUser.WithCustomMockUser;
+import kpl.fiml.project.domain.Project;
+import kpl.fiml.project.domain.ProjectRepository;
+import kpl.fiml.project.domain.enums.ProjectCategory;
+import kpl.fiml.project.domain.enums.ProjectStatus;
+import kpl.fiml.settlement.domain.Settlement;
+import kpl.fiml.settlement.domain.SettlementRepository;
+import kpl.fiml.user.domain.User;
+import kpl.fiml.user.domain.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -18,16 +30,58 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class SettlementControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private SettlementRepository settlementRepository;
 
     @Test
     @WithCustomMockUser
     @DisplayName("특정 프로젝트에 대한 정산 정보를 조회할 수 있다.")
-    public void testGetSettlementByProject() throws Exception {
+    void testGetSettlementByProject() throws Exception {
+        // given
+        User user = userRepository.save(createUser());
+        Project project = projectRepository.save(createProject(user));
+        Settlement settlement = settlementRepository.save(createSettlement(project));
+
+        // when-then
         mockMvc.perform(get("/api/v1/settlements/{projectId}", 1L))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.settleAmount").isNumber())
+                .andExpect(jsonPath("$.settleAmount").value(settlement.getSettleAmount()))
                 .andExpect(jsonPath("$.settledAt").isNotEmpty());
+    }
+
+    User createUser() {
+        return (User) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+    }
+
+    Project createProject(User user) {
+        Project project = Project.builder()
+                .summary("summary")
+                .category(ProjectCategory.ART)
+                .user(user)
+                .build();
+        ReflectionTestUtils.setField(project, "endAt", LocalDateTime.now().plusDays(1L));
+        ReflectionTestUtils.setField(project, "status", ProjectStatus.PROCEEDING);
+
+        return project;
+    }
+
+    Settlement createSettlement(Project project) {
+        return Settlement.builder()
+                .project(project)
+                .settleAmount(123456L)
+                .settledAt(LocalDateTime.now())
+                .build();
     }
 }
